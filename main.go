0fcf6f0738930f20e0ad2576648eb21be580ed11
Tomato Ketchup <r@554.jp>
2016-12-02 21:09:03 +0900

    Calendar のテンプレートを作成し表示するよう変更

--- main.go
+++ main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "Hello, World!")
+// 許容する年の範囲
+const (
+	yearMin = 2014
+	yearMax = 2099
+)
+
+// テンプレート
+var calendarTmpl = template.Must(template.ParseFiles("templates/calendar.tmpl"))
+
+// カレンダーに表示するデータを格納する構造体
+type calendarData struct {
+	Year int
+}
+
+// カレンダーを表示するハンドラー
+func calenerHundler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	var year int
+
+	if r.URL.Path == "/" {
+		if now.Month() >= 11 {
+			// 11月以降は今年
+			year = now.Year()
+		} else {
+			// 10月以前は去年
+			year = now.Year() - 1
+		}
+	} else {
+		// URL から年を取得
+		_, err := fmt.Sscanf(r.URL.Path, "/%d", &year)
+		if err != nil || year < yearMin || year > yearMax {
+			// 見つからない
+			w.WriteHeader(404)
+			fmt.Fprintln(w, "Not Found")
+			return
+		}
+	}
+	err := calendarTmpl.Execute(w, &calendarData{
+		Year: year,
 	})
+	if err != nil {
+		w.WriteHeader(500)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", calenerHundler)
 
 	http.ListenAndServe(":8080", nil)
 }
