af26a39b364c717f0abea9fdc881d7b96d8bdcb7
Tomato Ketchup <r@554.jp>
2016-12-06 19:36:52 +0900

    Google App Engine への対応
    App Engine へ対応するため、main() を削除し init のみ定義。
    
    app.xaml を追加

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"net/http"
+import _ "github.com/kechako/go-advent-calendar/calendar"
 
-	_ "github.com/kechako/go-advent-calendar/calendar"
-	"github.com/kechako/go-advent-calendar/router"
-)
-
-func main() {
-	http.ListenAndServe(":8080", router.Router)
+func init() {
 }
