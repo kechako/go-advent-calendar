48ee308fa5a168c8654e76e74b7dd6b9dfcac040
Tomato Ketchup <r@554.jp>
2016-12-11 10:55:57 +0900

    カレンダーにエントリーを表示するよう変更

--- calendar/calendar.go
+++ calendar/calendar.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kechako/go-advent-calendar/config"
 	"github.com/kechako/go-advent-calendar/router"
+	"github.com/kechako/go-advent-calendar/store"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
@@ -18,7 +19,9 @@ var calendarTmpl = template.Must(template.ParseFiles("templates/calendar.tmpl"))
 
 // 日を格納する構造体
 type calendarDay struct {
-	Day int
+	Day   int
+	Entry *store.Entry
+	Date  time.Time
 }
 
 // 週を格納する構造体
@@ -31,6 +34,7 @@ type calendarData struct {
 	Name  string
 	Year  int
 	Weeks []*calendarWeek
+	Now   time.Time
 }
 
 func init() {
@@ -50,11 +54,20 @@ func calendarHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	s := store.NewStore(ctx)
+	entries, err := s.GetEntries(year)
+	if err != nil {
+		log.Errorf(ctx, "Get entries error : %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 	// カレンダー用データを作成
 	data := &calendarData{
 		Name:  conf.CalendarName,
 		Year:  year,
-		Weeks: makeWeeks(year, conf.Location),
+		Weeks: makeWeeks(year, entries, conf.Location),
+		Now:   time.Now().In(conf.Location),
 	}
 
 	// 直接レスポンスに書き込むとエラー時にステータスを変更できないので
@@ -71,7 +84,9 @@ func calendarHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // year で指定された年の週データを作成します。
-func makeWeeks(year int, loc *time.Location) []*calendarWeek {
+func makeWeeks(year int, entries []*store.Entry, loc *time.Location) []*calendarWeek {
+	entMap := makeEntryMap(entries)
+
 	weeks := make([]*calendarWeek, 0, 5)
 	t := time.Date(year, 12, 1, 0, 0, 0, 0, loc)
 	for wi := 0; ; wi++ {
@@ -85,6 +100,8 @@ func makeWeeks(year int, loc *time.Location) []*calendarWeek {
 
 			if t.Weekday() == time.Weekday(di) {
 				day.Day = t.Day()
+				day.Entry = entMap[day.Day]
+				day.Date = t
 				t = t.AddDate(0, 0, 1)
 				if t.Month() != 12 {
 					return weeks
@@ -92,4 +109,13 @@ func makeWeeks(year int, loc *time.Location) []*calendarWeek {
 			}
 		}
 	}
+}
+
+// エントリーの日付をキーとするマップを作成します。
+func makeEntryMap(entries []*store.Entry) map[int]*store.Entry {
+	entMap := make(map[int]*store.Entry)
+	for _, e := range entries {
+		entMap[e.Day] = e
+	}
+	return entMap
 }
