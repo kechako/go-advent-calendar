75c671029d35403ca1911969d7fa9893cfd3703a
Tomato Ketchup <r@554.jp>
2017-07-03 20:49:24 +0900

    Go のバージョン表示を行う /version を追加

--- app/main.go
+++ app/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"io"
+	"net/http"
+	"runtime"
+
 	_ "github.com/kechako/go-advent-calendar/calendar"
 	_ "github.com/kechako/go-advent-calendar/entry"
 )
 
 func init() {
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, runtime.Version())
+	})
 }
