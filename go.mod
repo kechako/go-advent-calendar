c913043153d28d40fbd2e77b1b269caf15fef95c
Ryosuke Akiyama <r@554.jp>
2019-08-22 21:18:58 +0900

    Support GAE go1.1x and Modules

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kechako/go-advent-calendar
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v0.0.0-20170712042213-0a4f71a498b7
+	golang.org/x/net v0.0.0-20170711181219-f01ecb60fe38
+	google.golang.org/appengine v1.0.1-0.20170707203349-ad2570cd3913
+)
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"runtime"
 
 	_ "github.com/kechako/go-advent-calendar/calendar"
 	_ "github.com/kechako/go-advent-calendar/entry"
 )
 
-func init() {
+func main() {
 	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, runtime.Version())
 	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
